9a32534019100c5d61125299c681bd3707efa9bb
Sven Helmberger <fforw@gmx.de>
2018-10-21 20:23:09 +0200

    Implement simple data transformation DSL
    So the goal for this project is to extract structural information about
    composite components and process exports from the Babel AST.
    
    All the information is there, we just need to transform it. So i'm
    writing a simple object transformation DSL that we will use to
    do that transformation.
    
    It works with transform function:
    
        transform(node, rule)
    
    The node is a AST node or a value within such a node and rule is a rule
    object that determines what to do.
    
      - if it's true, copy that property from the node
      - if the rule value is a string, use that string
      - the rule value is a function, call the function with the node and
        use the result unless it is undefined
    
    Furthermore, we make sure to call the same rule multiple times for
    arrays and recurse into complex objects.
    
    Helpers
    =======
    
    We implement two general helper functions
    
        Swith({ [key], ruleMap })
    
    switches based on an object property (default "type") and choses the
    value of the rule key that matches that property or the "default" key
    if none match. If there is no "default" key the result is undefined and
    will be ignored.
    
        Any(ruleMap)
    
    works on maps and applies the given ruleMap to any key.
    
    This commit also adds test-cases for the DSL.

---
+++ src/transform.js
@@ -0,0 +1,174 @@
+function transformRecursive(node, rule, path)
+{
+
+    if (Array.isArray(node))
+    {
+        const count = node.length;
+        const out = new Array(count);
+
+        let i = 0;
+        let j = 0;
+        while (i < count)
+        {
+            const result = transformRecursive(node[i], rule, path.concat(i++));
+            if (result !== undefined)
+            {
+                out[j++] = result;
+            }
+        }
+        if (j < count)
+        {
+            return out.slice(0, j);
+        }
+        return out;
+    }
+
+    if (!rule)
+    {
+        throw new Error("Invalid rule prop: " + path.join(".") + " = " + JSON.stringify(rule));
+    }
+
+    if (rule === true)
+    {
+        return node;
+    }
+    else if (typeof rule === "string")
+    {
+        return rule;
+    }
+    else if (typeof rule === "function")
+    {
+        const result = rule(node);
+        return result;
+    }
+
+    if (!node || typeof node !== "object" )
+    {
+        throw new Error("Cannot recurse into non-object at " + path.join(".") + ": " + JSON.stringify(node));
+    }
+
+    const out = {};
+
+    // copy type property by default if present, might be overrwritten by rule
+    if (node.type && rule.type !== null)
+    {
+        out.type = node.type;
+    }
+
+
+    for (let key in rule)
+    {
+        if (rule.hasOwnProperty(key))
+        {
+            const ruleProp = rule[key];
+
+            if (key !== "type" || ruleProp !== null)
+            {
+                const result = transformRecursive(node[key], ruleProp, path.concat(key));
+                if (result !== undefined)
+                {
+                    out[key] = result;
+                }
+            }
+        }
+    }
+    return out;
+}
+
+/**
+ * Transforms an Babel AST to a simplified JSON structure based on a given rule object.
+ *
+ * The shape of the rule object defines the final JSON output. Properties of the source object are copied, transformed or ignored
+ * based on the rule property:
+ *
+ *  * true: keep as-is
+ *
+ *  * a string : replace property with that string
+ *
+ *  * a function : call the function with the source property and use the result as replacement. If the result is undefined, ignore the property.
+ *
+ *  * an object : recurse into source property with object as new rule object
+ *
+ * @param node      Babel AST node
+ * @param rule      Transform object
+ */
+function transform(node, rule)
+{
+    return transformRecursive(node, rule, ["root"]);
+}
+
+function getType(node)
+{
+    return node.type;
+}
+
+/**
+ * Switch function. Takes a map of choices. If the value equals one of the keys, the value is used as sub-rule, otherwise
+ * the property is ignored.
+ * 
+ * @param [key]          key to switch on,. Default is "type"
+ * @param switchRule
+ * @return {Function}
+ */
+function Switch(key, switchRule)
+{
+    if (arguments.length === 1)
+    {
+        switchRule = key;
+        key = "type"
+    }
+
+    //console.log("Switch", fn.toString(), switchRule.toString());
+
+    return function (node) {
+
+        const value = node[key];
+
+        let result;
+        if (switchRule.hasOwnProperty(value))
+        {
+            result = transformRecursive(node, switchRule[value], ["Switch('" + value + "')"]);
+        }
+        else if (switchRule.default)
+        {
+            result = transformRecursive(node, switchRule.default, ["Switch.default"])
+        }
+
+        if (result && !result[key])
+        {
+            result[key] = value;
+        }
+
+        return result;
+    }
+}
+
+/**
+ * Any Helper. Takes any key of one level and applies a given rule to all of them. Useful for map constructs.
+ *
+ * @param rule      rule for the map values
+ * @return {function(object)} function
+ */
+function Any(rule)
+{
+    return function (node) {
+
+        const out = {};
+
+        for (let key in node)
+        {
+            if (node.hasOwnProperty(key))
+            {
+                const value = node[key];
+                out[key] = transform(value, rule);
+            }
+        }
+        return out;
+    }
+}
+
+module.exports = {
+    transform: transform,
+    Switch: Switch,
+    Any: Any
+};
---
+++ test/transform.js
@@ -0,0 +1,536 @@
+const assert = require("power-assert");
+const {transform, Switch, Any} = require("../src/transform");
+
+describe("Transform", function () {
+
+    it("copies properties as is", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    name: "Adam",
+                    ignored: "bla",
+                    sub: {
+                        age: 23
+                    }
+                }, {
+                    name: true,
+                    sub: true
+                }
+            ),
+            {
+                name: "Adam",
+                sub: {
+                    age: 23
+                }
+            }
+        )
+    });
+    it("substitutes string rule values", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    name: "Adam",
+                    ignored: "bla",
+                    sub: {
+                        age: 23
+                    }
+                }, {
+                    name: "Bob"
+                }
+            ),
+            {
+                name: "Bob"
+            }
+        )
+    });
+
+    it("recurses into properties", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    name: "Adam",
+                    sub: {
+                        age: 23,
+                        ignored: "bla"
+                    }
+                }, {
+                    name: true,
+                    sub: {
+                        age: true
+                    }
+                }
+            ),
+            {
+                name: "Adam",
+                sub: {
+                    age: 23
+                }
+            }
+        )
+    });
+
+    it("transforms values", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    name: "Adam",
+                    sub: {
+                        age: 23
+                    }
+                }, {
+                    name: true,
+                    sub: {
+                        age: function (age) {
+                            return age * 2
+                        }
+                    }
+                }
+            ),
+            {
+                name: "Adam",
+                sub: {
+                    age: 46
+                }
+            }
+        )
+    });
+
+    it("supports direct transformation", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    name: "Adam",
+                    sub: {
+                        age: 23
+                    }
+                }, function (p) {
+                    p.name = p.name.toUpperCase();
+                    return p;
+                }
+            ),
+            {
+                name: "ADAM",
+                sub: {
+                    age: 23
+                }
+            }
+        )
+    });
+
+    it("transforms arrays", function () {
+
+        assert.deepEqual(
+            transform(
+                [
+                    {
+                        name: "Adam"
+                    },
+                    {
+                        name: "Berta"
+                    }
+                ], {
+                    name: function (name) {
+                        return name.toUpperCase();
+                    }
+                }
+            ),
+            [
+                {
+                    name: "ADAM"
+                },
+                {
+                    name: "BERTA"
+                }
+            ]
+        );
+
+        assert.deepEqual(
+            transform(
+                {
+                    people:
+                        [
+                            {
+                                name: "Adam"
+                            },
+                            {
+                                name: "Berta"
+                            }
+                        ]
+                }, {
+                    people: {
+                        name: function (name) {
+                            return name.toUpperCase();
+                        }
+                    }
+                }
+            ),
+            {
+                people: [
+                    {
+                        name: "ADAM"
+                    },
+                    {
+                        name: "BERTA"
+                    }
+                ]
+
+            }
+        )
+    });
+
+    it("filters undefined results from arrays", function () {
+
+        assert.deepEqual(
+            transform(
+                [
+                    {
+                        name: "Adam"
+                    },
+                    {
+                        name: "Berta"
+                    },
+                    {
+                        name: "Klaus"
+                    }
+                ], function (node) {
+                    const { name } = node;
+                    if (name !== "Klaus")
+                    return name.toUpperCase();
+                }
+            ),
+            [
+                "ADAM","BERTA"
+            ]
+        );
+
+    });
+
+    it("filters undefined results from Objects", function () {
+
+        assert.deepEqual(
+            transform({
+                a: "value"
+            },{
+                a: function (node) {}
+            }),
+            {
+            }
+        );
+
+    });
+
+    it("Switch()es based on type properties", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    people: [
+                        {
+                            type: "A",
+                            name: "Adam"
+                        },
+                        {
+                            type: "B",
+                            name: "Berta"
+                        }
+                    ]
+                }
+                , {
+                    people: Switch({
+                        A: {
+                            name: function (name) {
+                                return "*" + name;
+                            }
+
+                        },
+                        B: {
+                            name: true
+                        }
+                    })
+                }
+            ),
+            {
+                people: [
+                    {
+                        type: "A",
+                        name: "*Adam"
+                    },
+                    {
+                        type: "B",
+                        name: "Berta"
+                    }
+                ]
+            }
+        )
+    });
+
+    it("Switch()es based on other properties", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    people: [
+                        {
+                            type: "A",
+                            name: "Adam"
+                        },
+                        {
+                            type: "B",
+                            name: "Berta"
+                        }
+                    ]
+                }
+                , {
+                    people: Switch(
+                        function (p) {
+                            return p.name
+                        },
+                        {
+                            Adam: {
+                                type: true
+
+                            },
+                            Berta: {
+                                type: function (name) {
+                                    return "*" + name;
+                                }
+                            }
+                        }
+                    )
+                }
+            ),
+            {
+                people: [
+                    {
+                        type: "A"
+                    },
+                    {
+                        type: "*B"
+                    }
+                ]
+            }
+        )
+    });
+
+    it("Switch()es back to default", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    people: [
+                        {
+                            type: "A",
+                            name: "Adam"
+                        },
+                        {
+                            type: "B",
+                            name: "Berta"
+                        },
+                        {
+                            type: "CCC",
+                            name: "Clara"
+                        }
+                    ]
+                }
+                , {
+                    people: Switch({
+                        A: {
+                            name: function (name) {
+                                return "*" + name;
+                            }
+
+                        },
+                        B: {
+                            name: true
+                        },
+
+                        default: {
+                            name: function (name) {
+                                return name.toUpperCase();
+                            }
+                        }
+                    })
+                }
+            ),
+            {
+                people: [
+                    {
+                        type: "A",
+                        name: "*Adam"
+                    },
+                    {
+                        type: "B",
+                        name: "Berta"
+                    },
+                    {
+                        type: "CCC",
+                        name: "CLARA"
+                    }
+                ]
+            }
+        )
+    });
+
+    it("does not abide by falsy rules (except on 'type')", function () {
+
+        assert.throws(
+            function () {
+                transform(
+                    {
+                        type: "A",
+                        name: "Adam"
+                    }
+                    , false
+                )
+            },
+            /Invalid rule prop: root = false/
+        )
+
+        assert.throws(
+            function () {
+                transform(
+                    {
+                        people: [
+                            {
+                                type: "A",
+                                name: "Adam"
+                            }
+                        ]
+                    }
+                    , {
+                        people: {
+                            name: null
+                        }
+
+                    }
+                )
+            },
+            /Error: Invalid rule prop: root.people.0.name = null/
+        )
+    });
+
+    it("does not recurse into non-objects", function () {
+
+        assert.throws(
+            function () {
+                transform(
+                    {
+                        type: "A",
+                        name: "Adam"
+                    }
+                    , {
+                        type: {
+                            name: "B"
+                        }
+                    }
+                )
+            },
+            /Cannot recurse into non-object at root.type: "A"/
+        )
+
+    });
+
+    it("copies the 'type' property by default", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    type: "Person",
+                    name: "Adam",
+                    ignored: "bla",
+                    sub: {
+                        age: 23
+                    }
+                }, {
+                    name: true
+                }
+            ),
+            {
+                type: "Person",
+                name: "Adam"
+            }
+        )
+    });
+
+    it("doesn't copy the 'type' property if its null on the rule", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    people: [
+                        {
+                            type: "A",
+                            name: "Adam"
+                        },
+                        {
+                            type: "B",
+                            name: "Berta"
+                        }
+                    ]
+                }
+                , {
+                    people: Switch({
+                        A: {
+                            type: null,
+                            name: function (name) {
+                                return "*" + name;
+                            }
+
+                        },
+                        B: {
+                            type: null,
+                            name: true
+                        }
+                    })
+                }
+            ),
+            {
+                people: [
+                    {
+                        name: "*Adam"
+                    },
+                    {
+                        name: "Berta"
+                    }
+                ]
+            }
+        )
+    });
+
+
+    it("handles maps with Any()", function () {
+
+        assert.deepEqual(
+            transform(
+                {
+                    AAA: {
+                        name: "Adam"
+                    },
+                    BBB: {
+                        name: "Berta"
+                    }
+                }
+                ,
+                Any(
+                    {
+
+                        name: function(n) { return n.toUpperCase() }
+                    }
+                )
+            ),
+            {
+                AAA: {
+                    name: "ADAM"
+                },
+                BBB: {
+                    name: "BERTA"
+                }
+            }
+        )
+    });
+});
