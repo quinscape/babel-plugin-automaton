c25d0c6a5711d42462c36bec5c6ad47cdbb0f80b
Sven Helmberger <fforw@gmx.de>
2018-10-23 21:03:07 +0200

    Switch back to models.json
    Writing to the src dir within the js build causes a whole lot of
    problems and is not necessary in the end.
    
    We collect the data into the target dir model.json as temporary location
    and create a dedicated command to let that flow back into src.

--- webpack/AutomatonPlugin.js
+++ webpack/AutomatonPlugin.js
@@ -1,76 +1,31 @@
 const fs = require("fs");
-const path = require("path");
 //const Data = require("babel-plugin-automaton/data");
 const Data = require("../data");
-const shellJs = require("shelljs");
 
 function AutomatonPlugin(options)
 {
-    if (typeof options.appsPath !== "string")
+    if (typeof options.output !== "string")
     {
-        throw new Error("Need appsPath option.");
-    }
-
-    if (typeof options.modelPath !== "string")
-    {
-        throw new Error("Need modelPath option.");
+        throw new Error("Need output option.");
     }
 
     // console.log("OPTIONS", JSON.stringify(options, null, 2));
     this.options = options;
 }
 
-function writeFiles(out, callback)
+AutomatonPlugin.prototype.apply = function (compiler)
 {
-    const current = out[0].path;
+    const { output, debug } = this.options;
 
-    console.log("Writing model to ", current);
-
-    if (!fs.existsSync(current))
+    compiler.plugin("emit", function (compilation, callback)
     {
-        shellJs.mkdir("-p", path.resolve(current, "..") + "/");
-    }
+        var usageData = Data.get();
 
-    fs.writeFile(current, out[0].json, function () {
-
-            const newOut = out.slice(1);
-            if (newOut.length)
-            {
-                writeFiles(newOut, callback);
-            }
-            else
-            {
-                callback();
-            }
-        });
-}
-
-AutomatonPlugin.prototype.apply = function (compiler) {
-    const {appsPath, modelPath} = this.options;
-
-    //console.log(appsPath, modelPath);
-
-    compiler.plugin("emit", function (compilation, callback) {
-        const allModels = Data.get();
-
-        const out = [];
-        let i = 0;
-        for (let relativePath in allModels)
+        if (debug)
         {
-            if (allModels.hasOwnProperty(relativePath))
-            {
-                const data = allModels[relativePath];
-                out[i++] = {
-                    path: path.resolve(modelPath, relativePath + ".json"),
-                    json: JSON.stringify(data, null, 4)
-                }
-
-            }
+            console.log("Write data to " + output);
         }
-
-        //console.log(out);
-
-        writeFiles(out, callback);
+        fs.writeFile(output, JSON.stringify(usageData), callback);
     });
 };
 
