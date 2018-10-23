ca520a92e995203638bdc20ffe540c1fbd4d2b7c
Sven Helmberger <fforw@gmx.de>
2018-10-23 20:39:47 +0200

    Webpack plugin: Write to many files
    This changes our plugin to take two options, a appsPath option for the
    source JavaScript files and a modelPath option for the output path in the
    webapp folder, which will be /WEB-INF/automaton.
    
    This writes all collected entries into the same hierarchy as the
    Javascript files, only as JSON data and with JSON endings.
    
    The JSON data is meant to flow back into the generator to be considered
    by new generation runs that update the same JSON data.

--- webpack/AutomatonPlugin.js
+++ webpack/AutomatonPlugin.js
@@ -1,26 +1,76 @@
 const fs = require("fs");
+const path = require("path");
 //const Data = require("babel-plugin-automaton/data");
 const Data = require("../data");
+const shellJs = require("shelljs");
 
 function AutomatonPlugin(options)
 {
-    if (typeof options.output !== "string")
+    if (typeof options.appsPath !== "string")
     {
-        throw new Error("Need output option.");
+        throw new Error("Need appsPath option.");
+    }
+
+    if (typeof options.modelPath !== "string")
+    {
+        throw new Error("Need modelPath option.");
     }
 
     // console.log("OPTIONS", JSON.stringify(options, null, 2));
     this.options = options;
 }
 
-AutomatonPlugin.prototype.apply = function (compiler)
+function writeFiles(out, callback)
 {
-    var output = this.options.output;
+    const current = out[0].path;
 
-    compiler.plugin("emit", function (compilation, callback)
+    console.log("Writing model to ", current);
+
+    if (!fs.existsSync(current))
     {
-        var usageData = Data.get();
-        fs.writeFile(output, JSON.stringify(usageData), callback);
+        shellJs.mkdir("-p", path.resolve(current, "..") + "/");
+    }
+
+    fs.writeFile(current, out[0].json, function () {
+
+            const newOut = out.slice(1);
+            if (newOut.length)
+            {
+                writeFiles(newOut, callback);
+            }
+            else
+            {
+                callback();
+            }
+        });
+}
+
+AutomatonPlugin.prototype.apply = function (compiler) {
+    const {appsPath, modelPath} = this.options;
+
+    //console.log(appsPath, modelPath);
+
+    compiler.plugin("emit", function (compilation, callback) {
+        const allModels = Data.get();
+
+        const out = [];
+        let i = 0;
+        for (let relativePath in allModels)
+        {
+            if (allModels.hasOwnProperty(relativePath))
+            {
+                const data = allModels[relativePath];
+                out[i++] = {
+                    path: path.resolve(modelPath, relativePath + ".json"),
+                    json: JSON.stringify(data, null, 4)
+                }
+
+            }
+        }
+
+        //console.log(out);
+
+        writeFiles(out, callback);
     });
 };
 
