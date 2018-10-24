792f01c5ea0efd6c2237a58c3310a0e260641fa4
Sven Helmberger <fforw@gmx.de>
2018-10-24 22:47:02 +0200

    Implement multi-file writing
    Now that we created our own NPM script for it, we can just write
    directly into src/main/webapp after all.

--- webpack/AutomatonPlugin.js
+++ webpack/AutomatonPlugin.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 //const Data = require("babel-plugin-automaton/data");
 const Data = require("../data");
+const shelljs = require("shelljs");
 
 function AutomatonPlugin(options)
 {
@@ -13,19 +15,57 @@ function AutomatonPlugin(options)
     this.options = options;
 }
 
+function getDirOf(relativePath)
+{
+    const pos = relativePath.lastIndexOf("/");
+    if (pos < 0)
+    {
+        throw new Error("Could not find / in " + relativePath);
+    }
+    return relativePath.substring(0, pos + 1);
+}
+
 AutomatonPlugin.prototype.apply = function (compiler)
 {
     const { output, debug } = this.options;
 
     compiler.plugin("emit", function (compilation, callback)
     {
-        var usageData = Data.get();
+        const usageData = Data.get();
 
-        if (debug)
+        for (let relativePath in usageData)
         {
-            console.log("Write data to " + output);
+            if (usageData.hasOwnProperty(relativePath))
+            {
+                const data = usageData[relativePath];
+
+                const fullPath = path.resolve(output, relativePath) + ".json";
+
+                const dir = path.resolve(output, getDirOf(relativePath));
+
+                console.log("dir = ", dir, "fullPath = ", fullPath);
+
+                if (!fs.existsSync(dir))
+                {
+                    if (debug)
+                    {
+                        console.log("Create directory " + dir);
+                    }
+                    shelljs.mkdir("-p", dir);
+                }
+
+                if (debug)
+                {
+                    console.log("Write data to " + fullPath);
+                }
+
+                fs.writeFileSync(fullPath, JSON.stringify(data, null, 4), "UTF-8");
+
+
+            }
         }
-        fs.writeFile(output, JSON.stringify(usageData), callback);
+        callback();
+
     });
 };
 
